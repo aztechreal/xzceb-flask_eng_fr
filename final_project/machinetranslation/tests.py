43b40edfc66952cf7b74d02da5b85be8fa8340c9
aztechreal <apfrias@me.com>
2022-12-31 08:41:50 -0800

    Update tests.py

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,10 +6,11 @@ class Testtranslator(unittest.TestCase):
     def test_f2e(self):
         #self.assertIsNone(inputfrenchtext)
         self.assertEqual(frenchtoenglish('Bonjour'),'Hello')
+        self.assertNotEqual(frenchtoenglish('Bonjour'),'Bye')
     def test_e2f(self):
         #self.assertIsNone(inputenglishtext)
         self.assertEqual(englishtofrench('Hello'),'Bonjour')
-
+        self.assertNotEqual(englishtofrench('Hello'),'Adieu')
 if __name__=='__main__':
     unittest.main()
 
